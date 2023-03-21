8bb0f5ecba0a9c32ea13287df33d67697d68348f
Aqdas <aqdas.idris@gmail.com>
2023-03-21 10:27:52 +0530

    fixed EmployeeEntity

--- src/main/kotlin/Employee/data/EmployeeEntity.kt
+++ src/main/kotlin/Employee/data/EmployeeEntity.kt
@@ -26,6 +26,5 @@ object employee_table:org.jetbrains.exposed.sql.Table(){
     val employee_salary=long("employee_salary")
     val employee_joining_date= long("employee_joining_date")
     val employee_type=varchar("employee_type",10)
-
     override val primaryKey=PrimaryKey(employee_id)
 }
